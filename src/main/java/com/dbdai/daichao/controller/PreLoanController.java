2e7b73de775061fbc613a3be436722d268d4dc10
ddd <11111@163.com>
2018-05-03 18:19:14 +0800

    add 创建preLoan controller

---
+++ src/main/java/com/dbdai/daichao/controller/PreLoanController.java
@@ -0,0 +1,11 @@
+package com.dbdai.daichao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/preLoan", method = RequestMethod.POST)
+public class PreLoanController {
+
+}
